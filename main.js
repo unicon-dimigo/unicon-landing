5ffb85f2e9d15c38f901156bf3b4f08366c7ad17
엄서훈 <uhmtoto@eomseohun-ui-MacBookPro.local>
2020-02-25 08:32:33 +0900

    Image optimization, Image Preload function

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
-let current = 10000
+const frameStart = 100000000,
+    frameCount = 2062,
+    scrollSpeed = 2
+
+let currentFrame = frameStart
 
 function update () {
-    if (current > 12062) current = 12062
-    $('#main-img').attr('src', `./src/Main_${current}.png`)
+    if (currentFrame > frameStart + frameCount) currentFrame = frameStart + frameCount
+    if (currentFrame < frameStart) currentFrame = frameStart
+    $('#main-img').attr('src', `./src/Main_${currentFrame}.jpg`)
 }
+
+function framePreload (images) {
+    images.forEach(value => {
+        let image = new Image()
+        image.src = value
+    })
+}
+
+function scrollBehavior (action) {
+    for (let i=0; i<scrollSpeed; ++i) {
+        action()
+        update()
+    }
+}
+
+update()
+
+let frames = new Array()
+for (let i=frameStart; i<=frameStart + frameCount; ++i) {
+    frames.push(`./src/Main_${i}.jpg`)
+}
+framePreload(frames)
 
 $('.container').on('mousewheel', e => {
     const wheel = e.originalEvent.wheelDelta
 
     if (wheel < 0) { // scroll up
-        for (let i=0; i<3; ++i) {
-            current++
-            update()
+        for (let i=0; i<scrollSpeed; ++i) {
+            scrollBehavior(() => { currentFrame++ })
         }
     } else { // scroll down
-        for (let i=0; i<3; ++i) {
-            current--
-            update()
+        for (let i=0; i<scrollSpeed; ++i) {
+            scrollBehavior(() => { currentFrame-- })
         }
     }
-    
 })
